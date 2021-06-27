747c4659dc25e379bf09e29b5eeac45dea7ae537
1Roland <84271205+1Roland@users.noreply.github.com>
2021-06-27 06:52:57 +0400

    changes

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-function compute() {
+function compute()
+{
+    var principal = document.getElementById("principal").value;
 
-    let principal = document.getElementById("principal").value 
-    let interestRate = document.getElementById("rate").value
-    let years = document.getElementById("noYears").value
-    let interestEarned = principal * interestRate * years *.01 + Number(principal);// this is still wrong
-    let d = new Date();
-    let n = d.getFullYear();
-    let futureDate = Number(n) + Number(years); 
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
 
-    document.getElementById("answer1").innerHTML = "If you deposit $" + principal;
-    document.getElementById("answer2").innerHTML = "At a rate of " + interestRate + '%';
-    document.getElementById("answer3").innerHTML = "You'll recieve the amount of $" + interestEarned
-    document.getElementById("answer4").innerHTML = "In the year " + futureDate
-}    
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+    
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+}
+
+function SliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
+}
